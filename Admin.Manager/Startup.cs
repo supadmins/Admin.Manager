1351a5e303c6783e8e2c2a75f85210368b7defbb
devil <supadmins@163.com>
2018-01-24 16:50:06 +0800

    添加项目文件。

---
+++ Admin.Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Admin.Manager.Startup))]
+namespace Admin.Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
